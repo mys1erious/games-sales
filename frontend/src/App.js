6f37097db6402d68b9c72a95f2b290c0f038614a
mys1erious <subtletyzeros@gmail.com>
2022-09-03 19:15:47 +0200

    route for Home page and Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Header from "./features/core/components/Header";
+
 
 
 function App() {
 
     return (
-        <React.Fragment>
-            <div>Hello world</div>
-        </React.Fragment>
+        <Router>
+            <Header />
+            <div className="body-container">
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
