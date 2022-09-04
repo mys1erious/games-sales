110e889fb88b0b91bc55a77bedf6fb2a274c2c3b
mys1erious <subtletyzeros@gmail.com>
2022-09-04 20:12:40 +0200

    pagination for sales

--- backend/games_sales/sales/api/views.py
+++ backend/games_sales/sales/api/views.py
@@ -1,4 +1,6 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from rest_framework import status
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -6,8 +8,32 @@ from ..models import Sale
 from .serializers import SaleSerializer
 
 
-class SaleListAPIView(APIView):
+class SaleListAPIView(APIView, LimitOffsetPagination):
+    page_size = 3
+
     def get(self, request, *args, **kwargs):
-        sales = Sale.objects.all()
-        serializer = SaleSerializer(sales, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        query_params = request.query_params
+        sales = Sale.objects.all().order_by('id')
+
+        if 'page' in request.query_params:
+            page_number = query_params.get('page')
+        else:
+            page_number = 1
+
+        paginator = Paginator(sales, self.page_size)
+        try:
+            paginator_page = paginator.page(page_number)
+        except (PageNotAnInteger, EmptyPage) as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = SaleSerializer(
+            paginator_page,
+            many=True,
+            context={'request', request}
+        )
+
+        # Better way to get num_pages without passing it with each request?
+        return Response({
+            'sales': serializer.data,
+            'num_pages': paginator.num_pages
+        }, status=status.HTTP_200_OK)
