2afc005cbe59b9963198082878127580b8f28699
mys1erious <subtletyzeros@gmail.com>
2022-09-26 16:02:51 +0200

    feat: First version of sidebar with order by, genre, esrb rating, and year of release(reset doesnt work) handling

---
+++ frontend/src/features/sales/components/SalesFilterMainSidebar.js
@@ -0,0 +1,53 @@
+import React from "react";
+import {Box, List, ListItem, ListItemButton, ListItemText} from "@mui/material";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+
+import {BaseButton as Button} from "../../core/components/BaseButton";
+import {useSearchParams} from "react-router-dom";
+import {highlightedInitState} from "../../core/utils";
+
+
+// For each Field, need to add its values
+// Then from API
+const SalesFilterMainSidebar = (filterFields, toggleMain, toggleSub,
+                                setCurrPage, highlighted, setHighlighted) => {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const resetFilters = () => {
+        setHighlighted(highlightedInitState);
+
+        setCurrPage(1);
+        setSearchParams({page: '1'});
+    };
+
+    return(
+        <React.Fragment>
+        <Box role="presentation" minWidth="216px" width="25vw">
+            <Box padding="8px" height="48px"
+                 sx={{display: "flex", justifyContent: "space-between"}}>
+                <Button content={<><NavigateBeforeIcon /> Filter</>} sx={{width: "100px"}}
+                        onClick={toggleMain(false)}
+                />
+                <Button content="Reset all" color="error" sx={{width: "100px"}}
+                        onClick={() => resetFilters()}
+                />
+            </Box>
+            <List disablePadding sx={{borderTop: "1px solid gray", borderBottom: "1px solid gray"}}>
+                {filterFields.map((text) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton onClick={toggleSub(true, text)}>
+                            <ListItemText primary={text} secondary="current"/>
+                            <ListItemText align="right">All</ListItemText>
+                            <NavigateNextIcon />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+        </React.Fragment>
+    );
+};
+
+
+export default SalesFilterMainSidebar;
