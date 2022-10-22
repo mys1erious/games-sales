0728d5e9889622aa05846fee901286660d371ad8
mys1erious <subtletyzeros@gmail.com>
2022-10-22 10:57:50 +0200

    feat: ReportBuilder Page

--- frontend/src/pages/ReportBuilder.js
+++ frontend/src/pages/ReportBuilder.js
@@ -5,14 +5,14 @@ import {TextField} from "@mui/material";
 import {setFormState} from "features/core/utils";
 import {Button} from "features/core/components/Button";
 import ReportBody from "features/reports/components/ReportBody";
-import {initialReportData} from "features/reports/constants";
+import {initReportHeaders} from "features/reports/constants";
 import {postReport} from "features/reports/services";
 
 
-const createReportFile = (reportData) => {
+const createReportFile = (reportHeaders) => {
     const doc = document.implementation.createHTMLDocument('Report');
     copyMuiStyles(doc);
-    setHtmlReportData(doc, reportData);
+    setHtmlReportData(doc, reportHeaders);
     return doc;
 };
 
@@ -24,26 +24,6 @@ const copyMuiStyles = (doc) => {
     }
 };
 
-const setHtmlReportData = (doc, reportData) => {
-    const reportContainer = doc.createElement('div');
-
-    reportContainer.appendChild(buildReportHeader(
-        doc, reportData, 'name', 'Name')
-    );
-    reportContainer.appendChild(buildReportHeader(
-        doc, reportData, 'remarks', 'Remarks')
-    );
-    reportContainer.appendChild(getReportBody());
-
-    doc.body.appendChild(reportContainer);
-};
-
-const buildReportHeader = (doc, reportData, field, fieldRepr) => {
-    const reportField = doc.createElement('div');
-    reportField.textContent = `${fieldRepr}: ${reportData[field]}`;
-    return reportField;
-};
-
 const getReportBody = () => {
     const reportBody = document.createElement('div');
     reportBody.innerHTML = document.getElementById('reportBody').innerHTML;
@@ -51,8 +31,28 @@ const getReportBody = () => {
     return reportBody;
 };
 
-const previewReport = (reportData) => {
-    const doc = createReportFile(reportData);
+const buildReportHeader = (doc, reportHeaders, field, fieldRepr) => {
+    const reportField = doc.createElement('div');
+    reportField.textContent = `${fieldRepr}: ${reportHeaders[field]}`;
+    return reportField;
+};
+
+const setHtmlReportData = (doc, reportHeaders) => {
+    const reportContainer = doc.createElement('div');
+
+    reportContainer.appendChild(buildReportHeader(
+        doc, reportHeaders, 'name', 'Name')
+    );
+    reportContainer.appendChild(buildReportHeader(
+        doc, reportHeaders, 'remarks', 'Remarks')
+    );
+    reportContainer.appendChild(getReportBody());
+
+    doc.body.appendChild(reportContainer);
+};
+
+const previewReport = (reportHeaders) => {
+    const doc = createReportFile(reportHeaders);
 
     const win = window.open("", "Report",
         "toolbar=no,location=no,directories=no,status=no," +
@@ -66,54 +66,53 @@ const HTMLDocumentToBlob = (doc) => {
     return new Blob([doc.documentElement.innerHTML], {type: "text/plain"});
 };
 
-const saveReport = async(reportData) => {
-    let doc = createReportFile(reportData);
+const saveReport = async(reportHeaders) => {
+    let doc = createReportFile(reportHeaders);
     doc = HTMLDocumentToBlob(doc);
 
     const data = new FormData();
-    data.set('name', reportData.name);
+    data.set('name', reportHeaders.name);
     data.set('report_body', doc)
 
-    // implement alert
     try {
         await postReport(data);
         console.log('created');
     } catch (e) {
-        if (e.response.status === 400)
-            console.log(e.response.data)
+        console.log(e.response)
     }
 };
 
 
 const ReportBuilder = () => {
-    const [reportData, setReportData] = useState(initialReportData);
+    const [reportHeaders, setReportHeaders] = useState(initReportHeaders);
 
     return (
-        <div style={{width: "90vw", margin: "auto"}}>
-            <div style={{textAlign: "center", border: "1px solid gray",
-                marginBottom: "15px", paddingTop: "10px"}}>
-                <TextField variant="outlined" label="Name" name="name"
-                           fullWidth sx={{marginBottom: "15px"}}
-                           onChange={(e) => setFormState(e, reportData, setReportData)}
-                />
-                <TextField variant="outlined" label="Remarks" name="remarks"
-                           fullWidth sx={{marginBottom: "15px"}}
-                           multiline minRows={8}
-                           onChange={(e) => setFormState(e, reportData, setReportData)}
-                />
-            </div>
-
-            <ReportBody />
-
-            <div style={{marginTop: "10px", float: "right"}}>
-                <Button onClick={() => previewReport(reportData)}>
-                    Preview
-                </Button>
-                <Button color="success" onClick={() => saveReport(reportData)}>
-                    Save
-                </Button>
-            </div>
+        <>
+        <div style={{textAlign: "center",
+            marginBottom: "15px", paddingTop: "10px"
+        }}>
+            <TextField variant="outlined" label="Name" name="name"
+                       fullWidth sx={{marginBottom: "15px"}}
+                       onChange={(e) => setFormState(e, reportHeaders, setReportHeaders)}
+            />
+            <TextField variant="outlined" label="Remarks" name="remarks"
+                       fullWidth sx={{marginBottom: "15px"}}
+                       multiline minRows={8}
+                       onChange={(e) => setFormState(e, reportHeaders, setReportHeaders)}
+            />
         </div>
+
+        <ReportBody />
+
+        <div style={{marginTop: "10px", float: "right"}}>
+            <Button onClick={() => previewReport(reportHeaders)}>
+                Preview
+            </Button>
+            <Button color="success" onClick={() => saveReport(reportHeaders)}>
+                Save
+            </Button>
+        </div>
+        </>
     );
 };
 
