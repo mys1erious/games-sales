940f2cba9fb31922dcfefb23614b03d4516b3e8e
mys1erious <subtletyzeros@gmail.com>
2022-10-26 23:13:08 +0200

    added zip()

--- frontend/src/features/reports/utils.js
+++ frontend/src/features/reports/utils.js
@@ -4,6 +4,7 @@ export const nameShortener = (name, maxLen) => (
         : name
 );
 
-
 export const roundVal = (val) =>
     Math.round((val + Number.EPSILON) * 100) / 100;
+
+export const zip = (a1, a2) => a1.map((x, i) => [x, a2[i]]);
