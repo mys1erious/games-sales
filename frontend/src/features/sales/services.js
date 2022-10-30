8419303e4889931b829043047fafb67022debddd
mys1erious <subtletyzeros@gmail.com>
2022-10-30 22:41:12 +0100

    removed Edit/Delete functionality

--- frontend/src/features/sales/services.js
+++ frontend/src/features/sales/services.js
@@ -9,14 +9,6 @@ export const getSale = async(saleSlug) => {
     return await axiosInstance.get(`/sales/${saleSlug}/`);
 };
 
-export const editSale = async(saleSlug) => {
-    console.log('Soon');
-};
-
-export const deleteSale = async(saleSlug) => {
-   return await axiosInstance.delete(`/sales/${saleSlug}`);
-};
-
 export const getFilterFieldsData = async() => {
     return await axiosInstance.get('/sale-filters/');
 };
