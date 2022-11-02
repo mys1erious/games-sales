2f22a8b4357e6d57e03eeba1371109b3f21b9a3c
mys1erious <subtletyzeros@gmail.com>
2022-11-02 21:32:19 +0100

    fix: now redirects out of signin/signup page if signed in and redirects out of sign-out page if not signed in

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -3,9 +3,21 @@ import {Grid, Typography} from "@mui/material";
 import AuthGrid from "features/auth/components/AuthGrid";
 import SignInForm from "features/auth/components/SignInForm";
 import AuthCaptionLink from "features/auth/components/AuthCaptionLink";
+import {useNavigate} from "react-router-dom";
+import {useContext, useEffect} from "react";
+import {UserContext} from "../features/auth/UserContext";
 
 
 const SignIn = () => {
+    const navigate = useNavigate();
+    const {user} = useContext(UserContext);
+
+    useEffect(() => {
+        if (user.isLoggedIn)
+            navigate(-1);
+    }, [user]);
+
+
     return(
         <AuthGrid>
             <Grid item xs={12}>
