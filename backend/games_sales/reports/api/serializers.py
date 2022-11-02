42a4b614c77daea46ffca750cdba73275b62a499
mys1erious <subtletyzeros@gmail.com>
2022-11-02 21:22:18 +0100

    added report field to ReportSerializer that returns full url to report_body, added slug field

--- backend/games_sales/reports/api/serializers.py
+++ backend/games_sales/reports/api/serializers.py
@@ -6,13 +6,22 @@ from ..models import Report
 class ReportSerializer(serializers.ModelSerializer):
     username = serializers.CharField(read_only=True, source='user.email')
     created = serializers.DateTimeField(read_only=True, format="%Y-%m-%d %H:%M")
+    report = serializers.SerializerMethodField('get_report_url', read_only=True)
 
     class Meta:
         model = Report
-        fields = ['name', 'created', 'user', 'username', 'report_body']
+        fields = ['name', 'slug', 'username', 'created', 'report', 'user', 'report_body']
         extra_kwargs = {
-            'user': {'write_only': True}
+            'user': {'write_only': True},
+            'report_body': {'write_only': True},
+            'slug': {'read_only': True}
         }
+
+    def get_report_url(self, obj):
+        request = self.context.get('request')
+        if request:
+            url = obj.report_body.url
+            return request.build_absolute_uri(url)
 
     def validate(self, data):
         user = data.get('user')
