3b768c84a3f8d799849a17ead5c47c89905bdb78
mys1erious <subtletyzeros@gmail.com>
2022-11-03 10:37:20 +0100

    feat: Drawer representation of Header on 900px media breakpoint

--- frontend/src/features/core/components/Header.js
+++ frontend/src/features/core/components/Header.js
@@ -1,8 +1,8 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 
 import {
     AppBar, Toolbar, Typography,
-    FormControlLabel, Switch
+    FormControlLabel, Switch, useTheme, useMediaQuery, Box
 } from "@mui/material";
 
 import SalesSearchBar from "features/sales/components/SalesSearchBar";
@@ -12,6 +12,8 @@ import LinkButton from "./LinkButton";
 import Image from "./Image";
 
 import logoSrc from "assets/logo.svg";
+import HeaderDrawer from "./HeaderDrawer";
+
 
 
 const Header = ({isDarkTheme, setIsDarkTheme}) => {
@@ -20,40 +22,51 @@ const Header = ({isDarkTheme, setIsDarkTheme}) => {
     };
     const {user} = useContext(UserContext);
 
+    const theme = useTheme();
+    const showDrawer = useMediaQuery(theme.breakpoints.down('md'));
+
+    const DefaultView = () => (
+        <>
+        <Typography variant="h6" color="inherit">
+            <LinkButton to="/" startIcon={
+                <Image src={logoSrc} alt="Logo" maxHeight={24} maxWidth={32}/>}>
+                Home
+            </LinkButton>
+            <LinkButton to="/sales/">Sales</LinkButton>
+            {user.isLoggedIn
+                ? <LinkButton to="/reports/">Reports</LinkButton>
+                : null
+            }
+            <SalesSearchBar/>
+        </Typography>
+        <Typography variant="h6" color="inherit">
+            <FormControlLabel
+                control={<Switch checked={isDarkTheme} onChange={changeTheme}/>}
+                label="Dark Mode" labelPlacement="start"/>
+            {user.isLoggedIn
+                ?
+                <LinkButton to="/sign-out/">Sign Out</LinkButton>
+                :
+                <>
+                    <LinkButton to="/signin/">Sign In</LinkButton>
+                    <LinkButton to="/signup/">Sign Up</LinkButton>
+                </>
+            }
+        </Typography>
+        </>
+    );
+
     return (
         <header>
-        <AppBar position="sticky" color="default" elevation={0}>
-        <Toolbar sx={{justifyContent: "space-between"}}>
-            <Typography variant="h6" color="inherit">
-                <LinkButton to="/" startIcon={
-                    <Image src={logoSrc} alt="Logo" maxHeight={24} maxWidth={32}/>
-                }>
-                    Home
-                </LinkButton>
-                <LinkButton to="/sales/">Sales</LinkButton>
-                {user.isLoggedIn
-                    ? <LinkButton to="/reports/">Reports</LinkButton>
-                    : null
+            <AppBar position="sticky" color="default" elevation={0}>
+            <Toolbar sx={{justifyContent: "space-between"}}>
+                {showDrawer
+                    ? <HeaderDrawer isDarkTheme={isDarkTheme}
+                                    changeTheme={changeTheme}/>
+                    : <DefaultView />
                 }
-                <SalesSearchBar />
-            </Typography>
-
-            <Typography variant="h6" color="inherit">
-                <FormControlLabel
-                    control={ <Switch checked={isDarkTheme} onChange={changeTheme} /> }
-                    label="Dark Mode" labelPlacement="start"/>
-                {user.isLoggedIn
-                    ?
-                    <LinkButton to="/sign-out/">Sign Out</LinkButton>
-                    :
-                    <>
-                        <LinkButton to="/signin/">Sign In</LinkButton>
-                        <LinkButton to="/signup/">Sign Up</LinkButton>
-                    </>
-                }
-            </Typography>
-        </Toolbar>
-        </AppBar>
+            </Toolbar>
+            </AppBar>
         </header>
     )
 };
---
+++ frontend/src/features/core/components/HeaderDrawer.js
@@ -0,0 +1,85 @@
+import React, {useContext, useState} from "react";
+
+import {Box, Drawer, FormControlLabel, IconButton, Switch} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+
+import logoSrc from "assets/logo.svg";
+import SalesSearchBar from "features/sales/components/SalesSearchBar";
+import {UserContext} from "features/auth/UserContext";
+
+import {Button} from "./Button";
+import BaseLinkButton from "./LinkButton";
+import Image from "./Image";
+
+
+const LinkButton = ({to, startIcon, children}) => (
+    <Box width="100%" textAlign="center"
+         border="1px solid gray" borderRadius={10}
+         marginBottom="4px"
+    >
+        <BaseLinkButton to={to} startIcon={startIcon}>
+            {children}
+        </BaseLinkButton>
+    </Box>
+);
+
+
+const HeaderDrawer = ({isDarkTheme, changeTheme}) => {
+    const {user} = useContext(UserContext);
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <>
+        <Box whiteSpace="nowrap">
+            <IconButton
+                size="small"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                onClick={() => setIsOpen(true)}>
+                <MenuIcon />
+            </IconButton>
+            <SalesSearchBar/>
+        </Box>
+        <Drawer anchor="left" open={isOpen} sx={{position: "relative"}}
+                onClose={() => setIsOpen(false)}>
+            <Box padding="8px" height="48px">
+                <Button onClick={()=> setIsOpen(false)}>
+                    <NavigateBeforeIcon/>
+                </Button>
+                <FormControlLabel
+                    control={<Switch checked={isDarkTheme} onChange={changeTheme}/>}
+                    label="Dark Mode" labelPlacement="start"/>
+            </Box>
+            <Box margin="8px">
+                <Box minWidth="240px" width="25vw">
+                    <LinkButton to="/" startIcon={
+                        <Image src={logoSrc} alt="Logo" maxHeight={24} maxWidth={32}/>}>
+                        Home
+                    </LinkButton>
+                    <LinkButton to="/sales/">Sales</LinkButton>
+                    {user.isLoggedIn
+                        ? <LinkButton to="/reports/">Reports</LinkButton>
+                        : null
+                    }
+                </Box>
+                <Box minWidth="240px" width="25vw" position="absolute" bottom={10}>
+                    {user.isLoggedIn
+                        ?
+                        <LinkButton to="/sign-out/">Sign Out</LinkButton>
+                        :
+                        <>
+                            <LinkButton to="/signin/">Sign In</LinkButton>
+                            <LinkButton to="/signup/">Sign Up</LinkButton>
+                        </>
+                    }
+                </Box>
+            </Box>
+        </Drawer>
+        </>
+    )
+};
+
+
+export default HeaderDrawer;
