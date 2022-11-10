e1e532991d75b0a357c94934a8ce01f56e339517
mys1erious <subtletyzeros@gmail.com>
2022-11-10 09:24:04 +0100

    feat: now uses aws S3 storage for media files

--- backend/config/settings/paprod.py
+++ backend/config/settings/paprod.py
@@ -6,7 +6,6 @@ from .base import *
 DEBUG = False
 ALLOWED_HOSTS = ['mysterious.pythonanywhere.com']
 
-# For now
 CORS_ALLOWED_ORIGINS = [
     'https://games-sales.mys1erious.com',
     'https://games-sales-mys1erious.vercel.app'
@@ -27,9 +26,21 @@ DATABASES = {
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'games_sales', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'collectedstatic')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'games_sales', 'media')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'games_sales', 'media')
+
+AWS_ACCESS_KEY_ID = get_env_var('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = get_env_var('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = get_env_var('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_DEFAULT_ACL = 'public-read'
+
+DEFAULT_FILE_STORAGE = 'core.storages.MediaS3Storage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+# STATICFILES_STORAGE = 'core.storages.StaticS3Storage'
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 
 SPECTACULAR_SETTINGS['SERVERS'] = [
